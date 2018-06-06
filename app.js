53dbfa9a287dd574bd224b5b65a90965679b884b
Meskjei <513087078@qq.com>
2018-06-06 18:25:30 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -2,16 +2,28 @@ App({
   globalData:{
     tableID: {
       tips: '37869',
-      userTaskList: '38593'
+      userTaskList: '38593',
+      follow: '38936',
+      isVerified: '39061'
     },
     tipsData:[],
     currentTaskTipID: '',
-    currentTip: {}
+    currentTip: {},
+    systemInfo: {},
+    currentUserInfo: {}
   },
   /**
    * 当小程序初始化完成时，会触发 onLaunch（全局只触发一次）
    */
   onLaunch: function () {
+    try {
+      let info = wx.getSystemInfoSync();
+      this.globalData.systemInfo = info;
+    } catch (e) {
+      wx.showToast({
+        title: '获取系统信息出错',
+      });
+    }
     require('./libs/sdk-v1.4.0');
     let clientID = 'fe4e7217a0ff01da81f8';
     let ret = wx.BaaS.init(clientID);
